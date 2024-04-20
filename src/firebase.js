d62f29228bd66966cca2cc2776d4433e15eff00c
Samuel Velasquez <samuelvelasquez@protonmail.com>
2024-04-19 22:18:01 -0400

    temp setup for activity

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBThVZi7vuuhLs-O3RxWzjc8UpCF2-G-2g",
+  authDomain: "grad-gcloud-project.firebaseapp.com",
+  projectId: "grad-gcloud-project",
+  storageBucket: "grad-gcloud-project.appspot.com",
+  messagingSenderId: "438979725475",
+  appId: "1:438979725475:web:8a9da6dea3725213139716"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
